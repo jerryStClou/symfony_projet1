a30f892eec3f9009e007c933b32f1cedf6f5da3b
Jerry <jerry.clou@outlook.com>
2023-07-19 16:54:21 +0200

    Mercredi 19 Juillet 2023

--- src/Controller/EmployesController.php
+++ src/Controller/EmployesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Employes;
 use App\Form\EmployeType;
 use App\Repository\EmployesRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -35,13 +36,54 @@ class EmployesController extends AbstractController
 
             $entitymanager->persist($employes);
             $entitymanager->flush();
+            $this->redirectToRoute('app_employes');
         }
 
 
         return $this->render('employes/new.html.twig', [
             'form' => $form
-
-
         ]);
+        return $this->redirectToRoute('app_employes');
+    }
+
+
+
+    #[Route('/employes/edit/{id}', name: 'app_employes_edit')]
+    public function edit(EmployesRepository $employesRepository, $id, Request $request, EntityManagerInterface $entityManagerInterface): Response
+    {
+        $employes = $employesRepository->find($id);
+
+        $form = $this->createForm(EmployeType::class, $employes);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $entityManagerInterface->persist($employes);
+            $entityManagerInterface->flush();
+            $this->redirectToRoute('app_employes');
+        }
+
+        return $this->render('employes/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+
+    #[Route('/employes/delete/{id}', name: 'app_employes_delete')]
+    public function delete($id, EmployesRepository $employesRepository, EntityManagerInterface $entityManagerInterface)
+    {
+        $employes = $employesRepository->find($id);
+
+        $entityManagerInterface->remove($employes);
+        $entityManagerInterface->flush();
+
+        return $this->redirectToRoute('app_employes');
+    }
+
+
+    #[Route('/employes/show/{id}', name: 'app_employes_show')]
+    public function show(EmployesRepository $employesRepository, $id): Response
+    {
+        $employes = $employesRepository->find($id);
+        return $this->render('employes/show.html.twig', ['employe' => $employes]);
     }
 }
